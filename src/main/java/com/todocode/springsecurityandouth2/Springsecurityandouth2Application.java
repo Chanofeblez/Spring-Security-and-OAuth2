06cfdec57bc1dc43edca31a304071b74bfdf4d4c
Julio Feblez <chano.feblez@yahoo.com>
2024-06-30 05:52:34 -0700

    Basic Settings Ready

---
+++ src/main/java/com/todocode/springsecurityandouth2/Springsecurityandouth2Application.java
@@ -0,0 +1,13 @@
+package com.todocode.springsecurityandouth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityandouth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurityandouth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/todocode/springsecurityandouth2/Springsecurityandouth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.springsecurityandouth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurityandouth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
